4cdaa77c737fe1ccaf4a4dc766c0a176af5b1131
SelectPerson <vovalutsk961@gmail.com>
2022-11-03 18:54:40 +0200

    [+] counter files number value

---
+++ lesson_2/sumFiles.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const dirName = "./files";
+
+fs.readdir(dirName, function(err, filenames) {
+  let files = [];
+
+  filenames.forEach(elem => files.push(`${dirName}/${elem}`));
+
+  const readFileOfNumber = ((value) => {
+    console.log("value", value)
+    const result = fs.readFileSync(value) * 1;
+    if(isNaN(result)) return 0;
+    return result;
+  });
+
+  const collectArrNumbers = [];
+  files.forEach((elem) => collectArrNumbers.push(readFileOfNumber(elem)));
+
+  const sum = collectArrNumbers.reduce((previousValue, currentValue) => previousValue + currentValue);
+
+  console.log("sum", sum);
+});
+
+

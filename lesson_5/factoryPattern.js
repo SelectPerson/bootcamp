88b685e68a74941216f362f0ebcb75644e6866d8
SelectPerson <vovalutsk961@gmail.com>
2022-11-17 18:07:38 +0200

    started create pattern factory v3

--- lesson_5/factoryPattern.js
+++ lesson_5/factoryPattern.js
@@ -1,55 +1,60 @@
 var IosCourse = /** @class */ (function () {
-    function IosCourse(_a) {
-        var duration = _a.duration;
+    function IosCourse(duration) {
+        if (duration === void 0) { duration = 4000; }
         this.name = "Ios" /* CourseName.Ios */;
-        this.duration = duration || 3600;
+        this.duration = duration;
         this.teacher = 'Alex';
         this.city = 'Kyiv';
     }
     return IosCourse;
 }());
 var AndroidCourse = /** @class */ (function () {
-    function AndroidCourse(_a) {
-        var duration = _a.duration, teacher = _a.teacher;
-        this.name = 'Android';
+    function AndroidCourse(duration) {
+        if (duration === void 0) { duration = 2000; }
+        this.name = "Android" /* CourseName.Android */;
         this.duration = duration;
-        this.teacher = teacher;
+        this.teacher = 'Ivan';
+        this.city = 'Kyiv';
     }
     return AndroidCourse;
 }());
 var ReactCourse = /** @class */ (function () {
-    function ReactCourse(_a) {
-        var duration = _a.duration, teacher = _a.teacher;
-        this.name = 'React';
+    function ReactCourse(duration) {
+        if (duration === void 0) { duration = 3000; }
+        this.name = "React" /* CourseName.React */;
         this.duration = duration;
-        this.teacher = teacher;
+        this.teacher = 'Volodymyr';
+        this.city = 'Lutsk';
     }
     return ReactCourse;
 }());
 var NestCourse = /** @class */ (function () {
-    function NestCourse() {
+    function NestCourse(duration) {
+        if (duration === void 0) { duration = 3600; }
+        this.name = "NestJS" /* CourseName.NestJS */;
+        this.duration = duration;
+        this.teacher = 'Roman';
+        this.city = 'Lutsk';
     }
     return NestCourse;
 }());
 var CourseFactory = /** @class */ (function () {
     function CourseFactory() {
     }
-    CourseFactory.prototype.create = function (type) {
+    CourseFactory.prototype.create = function (type, duration) {
         switch (type) {
-            case "Ios" /* CourseName.Ios */: return new IosCourse({ city: undefined, name: undefined, teacher: undefined, duration: 3000 });
+            case "Ios" /* CourseName.Ios */: return new IosCourse(duration);
+            case "Android" /* CourseName.Android */: return new AndroidCourse(duration);
+            case "React" /* CourseName.React */: return new ReactCourse(duration);
+            case "NestJS" /* CourseName.NestJS */: return new NestCourse(duration);
         }
-    };
-    CourseFactory.prototype.getCourse = function (_a) {
-        var type = _a.type;
-        console.log("");
-    };
-    CourseFactory.list = {
-        ios: IosCourse,
-        android: AndroidCourse,
-        react: ReactCourse,
-        nest: NestCourse
     };
     return CourseFactory;
 }());
 var courses = new CourseFactory();
-courses.create("Ios" /* CourseName.Ios */);
+var ios = courses.create("Ios" /* CourseName.Ios */, 5000);
+console.log("ios", ios);
+var react = courses.create("Ios" /* CourseName.Ios */);
+console.log("ios", react);
+var nestjs = courses.create("NestJS" /* CourseName.NestJS */);
+console.log("nestjs", nestjs);

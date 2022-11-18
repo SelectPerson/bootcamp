68434113a60e40ef7423b05c98c6c5ede53f64a8
SelectPerson <vovalutsk961@gmail.com>
2022-11-18 21:46:01 +0200

    refacted sum v2

--- lesson_6/P_2/sum.test.js
+++ lesson_6/P_2/sum.test.js
@@ -2,6 +2,7 @@ const sum = require('./sum');
 
 test('adds sum', () => {
     const result = sum(1, 2);
+    expect(result).toEqual(expect.any(Number));
     expect(result).toBeDefined();
     expect(result).not.toBeNull();
     expect(result).not.toBeUndefined();
